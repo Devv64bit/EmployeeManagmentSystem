91361c5a31c9cea915cb4a6863b58cb181d9369d
Dev <pateldev7v@gmail.com>
2020-10-26 22:27:50 -0400

    final changes. add comments so people can understand.

--- src/Employee.cpp
+++ src/Employee.cpp
@@ -27,7 +27,7 @@ static vector <string> empDetails;
 static int dataNum = 0;
 static void displayInfo();
 static void getInfo(string str);
-static void userAuth();
+static void userAuthentication();
 static void enterEmployeeDetails();
 static void deleteInfo();
 static void knowInfo(string emailId);
@@ -35,13 +35,13 @@ static void displayInfo();
 static void modifyInfo(string str);
 static void replaceInfo(string str);
 
-int main()
-{
-    userAuth();
+int main() {
+    // call this method to differentiate between the admin and user.
+    // Then, it allows them to do things provided to them.
+    userAuthentication();
 }
 
-static void userAuth()
-{
+static void userAuthentication() {
     string input;
     string input1;
     cout << "Are you an admin or employee: ";
@@ -51,61 +51,61 @@ static void userAuth()
     cin >> input;
 
     // only allow the admin to login with this password check
-    if (input == "adminiscool")
-    {
-        cout << "\nYou succesfully logged in as admin ";
+    if (input == "adminiscool") {
+        cout << "You succesfully logged in as ADMIN ";
+        cout << "\n";
         cout << "\nWhat do you want to do: " << endl;
+        cout << "*******************************" << endl;
         cout << "Choice 1: Add an Employee Detail \nChoice 2: Modify an Employee Detail \nChoice 3: Know an Employee Detail \nChoice 4: Delete \n->";
         cin >> input1;
 
         // nested if statement when the admin choses an option
-        if (input1 == "1")
-        {
+        if (input1 == "1") {
+            // call function to enter employee details.
             enterEmployeeDetails();
         }
 
-        if (input1 == "2")
-        {
+        if (input1 == "2") {
+            // call function to modify employee details.
             displayInfo();
         }
 
-        if (input1 == "3")
-        {
+        if (input1 == "3") {
+            // call function to know employee details.
             knowInfo(emailId);
         }
 
-        if (input1 == "4")
-        {
+        if (input1 == "4") {
+            // call function to delete employee
             deleteInfo();
         }
     }
     // any employees can login with the password given
-    else if (input == "cmpsc121")
-    {
-        cout << "You succesfully logged in as a employee ";
-        cout << "\nWhat do you want to do: " << endl;
-        cout << "Choice 1: Add an employee detail \nChoice 2: Know an Employee Detail \n->";
+    else if (input == "cmpsc121") {
+        cout << "You succesfully logged in as a EMPLOYEE ";
+        cout << "\n";
+        cout << "What do you want to do: " << endl;
+        cout << "*******************************" << endl;
+        cout << "Choice 1: Add an Employee Detail \nChoice 2: Know an Employee Detail \n->";
         cin >> input1;
         // nested if statement when the employee choses an option
-        if (input1 == "1")
-        {
+        if (input1 == "1") {
+            // call function to enter employee details.
             enterEmployeeDetails();
         }
 
-        else if (input1 == "2")
-        {
+        else if (input1 == "2") {
+            // call function to know employee details.
             knowInfo(emailId);
         }
     }
-    else
-    {
-        cout << "Password is incorrect, try again";
+    else {
+        cout << "\nUser name or Password is incorrect, try again";
         exit(1);
     }
 }
 
-static void enterEmployeeDetails()
-{
+static void enterEmployeeDetails() {
     cout << "\nFirst Name: " << endl;
     cin >> firstName;
     cout << "\n";
@@ -158,33 +158,32 @@ static void enterEmployeeDetails()
     cin >> vehicleTagNumber;
     cout << "\n";
 
+    // instantiate ofstream object to open txt file
     ofstream myFile("employeesInformation.txt", ios::app);
+
+    // a string variable to concatenate to all the information into one line.
     string alltheInfo = "\n" + firstName + " " + lastName + " " + emailId + " " + phoneNumber + " " + aptNumber + " " + streetAddress + " " + city + " " + state + " " + zipCode + " " + vehicleType + " " + vehicleModel + " " + vehicleColor + " " + vehicleTagNumber + " ";
 
-    // instantiate ofstream object to open txt file
     myFile << alltheInfo;
     myFile.close();
 
     cout << "\nDone entering the information into the file";
-    exit(0);
+    exit(1);
 }
 
-static void deleteInfo()
-{
+static void deleteInfo() {
     string input;
-    cout << "Enter the name of the employee you want to delete"
-         << "\n"
-         << endl;
+    cout << "Enter the name of the employee you want to delete"<< "\n" << endl;
     cin >> input;
 
     string findName;
     ifstream myFile("employeesInformation.txt");
     ofstream makeCopy("example.txt");
 
-    while (getline(myFile, findName))
-    {
-        if (findName.substr(0, input.size()) != input)
-        {
+    while (getline(myFile, findName)) {
+        // substr method to find where the name is the txt file and delete it.
+
+        if (findName.substr(0, input.size()) != input) {
             makeCopy << findName << endl;
         }
     }
@@ -194,10 +193,9 @@ static void deleteInfo()
     rename("example.txt", "employeesInformation.txt");
 }
 
-static void knowInfo(string email)
-{
-
-    cout << "\nEnter an email to know employee details: " << endl;
+static void knowInfo(string email) {
+    cout << "\nPlease enter the exact email given in the txt file"<<endl;
+    cout << "Enter an email to know employee details: " << endl;
     cin >> email;
     string employeeDetails[] =
         {
@@ -208,107 +206,96 @@ static void knowInfo(string email)
             "First & Last Name: Pat Cummins\nVehicle Type: SUV\nVehicle Model: 2020 Acura RDX\nVehicle Color: Dark Blue\nVehicle Tag Number: 5432",
             "First & Last Name: Kieron Pollard\nVehicle Type: SUV\nVehicle Model: 2018 Jeep Wrangler\nVehicle Color: Red\nVehicle Tag Number: 7402",
             "First & Last Name: Virat Kohli\nVehicle Type: Sedan\nVehicle Model: 2020 Audi R8\nVehicle Color: Yellow\nVehicle Tag Number: 4201",
-            "First & Last Name: Chris Gayle\nVehicle Type: SUV\nVehicle Model: 2019 Nissan Rouge\nVehicle Color: Black\nVehcle Tag Number : 3030"
+            "First & Last Name: Chris Gayle\nVehicle Type: SUV\nVehicle Model: 2019 Nissan Rouge\nVehicle Color: Black\nVehicle Tag Number : 3030",
+            "First & Last Name: Jonny Baristow\nVehicle Type: Sedan\nVehicle Model: 2013 Mercedes Benz SL500\nVehicle Color: Orange\nVehicle Tag Number : 9738",
 
         };
 
-    if (email == "dpatel@gmail.com")
-    {
+    if (email == "dpatel@gmail.com") {
+        // call the array index for each email and print the info.
+
         cout << employeeDetails[0] << endl;
     }
-    else if (email == "mr360@gmail.com")
-    {
+    else if (email == "mr360@gmail.com") {
         cout << employeeDetails[1];
     }
-    else if (email == "JA@gmail.com")
-    {
+    else if (email == "JA@gmail.com") {
         cout << employeeDetails[2];
     }
-    else if (email == "DW@gmail.com")
-    {
+    else if (email == "DW@gmail.com") {
         cout << employeeDetails[3];
     }
-    else if (email == "PC@gmail.com")
-    {
+    else if (email == "PC@gmail.com") {
         cout << employeeDetails[4];
     }
-    else if (email == "bigboss@gmail.com")
-    {
+    else if (email == "bigboss@gmail.com") {
         cout << employeeDetails[5];
     }
-    else if (email == "VK@gmail.com")
-    {
+    else if (email == "VK@gmail.com") {
         cout << employeeDetails[6];
     }
-    else if (email == "universeboss@gmail.com")
-    {
+    else if (email == "universeboss@gmail.com") {
         cout << employeeDetails[7];
+    } 
+    else if(email == "JB@gmail.com"){
+        cout << employeeDetails[8];
+    }
+    else {
+        cout << "Email not found in Database";
+        exit(1);
     }
 }
 
-static void displayInfo()
-{
+static void displayInfo() {
     string input;
-    cout << "Enter the first name of the employee you would like to modify :";
+    cout << "Enter the first name of the employee you would like to modify : ";
     cin >> input;
 
-    cout << "Please type the employee data you would like to modify: \"First_Name\", \"Last_Name\", \"Email_ID\", \"Phone_Number\", \"Apartment_Number\", \"Street_Address\", \"City\", \"State\", \"Zipcode\", \"Vehicle_Type\", \"Vehicle_Model\", \"Vehicle_Color\", or \"Vehicle_Tag_Number\".\n";
+    cout << "\nPlease type the employee data you would like to modify: \"First_Name\", \"Last_Name\", \"Email_ID\", \"Phone_Number\", \"Apartment_Number\", \"Street_Address\", \"City\", \"State\", \"Zipcode\", \"Vehicle_Type\", \"Vehicle_Model\", \"Vehicle_Color\", or \"Vehicle_Tag_Number\".\n";
     cin >> empData;
 
-    if (empData == "First_Name")
-    {
+    if (empData == "First_Name") {
+        // dataNum will set the index to the given input
+        // eg: First name is at 0 index in the txt file.
         dataNum = 0;
     }
-    else if (empData == "Last_Name")
-    {
+    else if (empData == "Last_Name") {
         dataNum = 1;
     }
-    else if (empData == "Email_ID")
-    {
+    else if (empData == "Email_ID") {
         dataNum = 2;
     }
-    else if (empData == "Phone_Number")
-    {
+    else if (empData == "Phone_Number") {
         dataNum = 3;
     }
-    else if (empData == "Apartment_Number")
-    {
+    else if (empData == "Apartment_Number") {
         dataNum = 4;
     }
-    else if (empData == "Street_Address")
-    {
+    else if (empData == "Street_Address") {
         dataNum = 5;
     }
-    else if (empData == "City")
-    {
+    else if (empData == "City") {
         dataNum = 6;
     }
-    else if (empData == "State")
-    {
+    else if (empData == "State") {
         dataNum = 7;
     }
-    else if (empData == "Zipcode")
-    {
+    else if (empData == "Zipcode") {
         dataNum = 8;
     }
-    else if (empData == "Vehicle_Type")
-    {
+    else if (empData == "Vehicle_Type") {
         dataNum = 9;
     }
-    else if (empData == "Vehicle_Model")
-    {
+    else if (empData == "Vehicle_Model") {
         dataNum = 10;
     }
-    else if (empData == "Vehicle_Color")
-    {
+    else if (empData == "Vehicle_Color") {
         dataNum = 11;
     }
-    else if (empData == "Vehicle_Tag_Number")
-    {
+    else if (empData == "Vehicle_Tag_Number") {
         dataNum = 12;
     }
-    else
-    {
+    else {
         cout << "Not a valid detail\n";
         exit(1);
     }
@@ -316,10 +303,13 @@ static void displayInfo()
     fstream newFile;
     newFile.open("employeesInformation.txt", ios::in);
     
-    if(newFile.is_open()){
+    if(newFile.is_open()) {
         string indexFinder;
-        while(getline(newFile, indexFinder)){
-            if(indexFinder.find(input) == 0){
+        while(getline(newFile, indexFinder)) {
+            // use find function to find exactly where the input from user is located.
+
+            if(indexFinder.find(input) == 0) {
+                // call this method store and locate the input.
                 modifyInfo(indexFinder);
             }
         }
@@ -333,8 +323,11 @@ static void displayInfo()
     newFile.open("employeesInformation.txt", ios::in);
     if(newFile.is_open()) {
         string indexFinder;
-        while(getline(newFile, indexFinder)){
-            if(indexFinder.find(input) ==0){
+        while(getline(newFile, indexFinder)) {
+            // use find function to find exactly where the input from user is located.
+
+            if(indexFinder.find(input) ==0) {
+                // call this method to perform the replacement of the input
                 replaceInfo(indexFinder);
             }
         }
@@ -345,42 +338,49 @@ static void displayInfo()
     ifstream myFile("employeesInformation.txt");
     ofstream makeCopy("example.txt");
 
-    while (getline(myFile, findName))
-    {
-        if (findName.substr(0, input.size()) != input)
-        {
+    while (getline(myFile, findName)) {
+        if (findName.substr(0, input.size()) != input) {
             makeCopy << findName << endl;
         }
     }
     myFile.close();
     makeCopy.close();
+    // remove and create a temp txt file to perform the replacement.
     remove("employeesInformation.txt");
+    // rename it so that the temp txt file becomes the new one.
     rename("example.txt", "employeesInformation.txt");
     ofstream outFile("employeesInformation.txt", ios_base::app);
     outFile<< newData;
 }
 
-static void modifyInfo(string str)
-{
+static void modifyInfo(string str) {
     string index;
     stringstream searcher(str);
 
-    while(searcher >> index){
+    // while loop to search for the index
+    while(searcher >> index) {
+        // push back the vector when index is found.
         empDetails.push_back(index);
     }
+
+    // fancy variable to say what the current item is in the txt file
+    // eg: Current zip code is 19020. 
     elementToMod = empDetails[dataNum];
 }
 
-static void replaceInfo(string str)
-{
-    string element;
+static void replaceInfo(string str) {
+    string index;
     stringstream searcher(str);
 
-    while(searcher >> element){
-        if(element!=elementToMod){
-            empDetails.push_back(element);
-            newData+=element + " ";
-        } else {
+    while(searcher >> index) {
+        // if statement to check that new index doesn't equal the old index.
+        if(index!=elementToMod) {
+            // push back the vector when the replacement is done.
+            empDetails.push_back(index);
+            // newData variable to compute the index and replace it.
+            newData+=index + " ";
+        } 
+        else {
             newData+=modElement + " ";
         }
     }
